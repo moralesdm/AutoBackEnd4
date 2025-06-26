0bdee7b1525d7d2b2163c9712b68a93855795622
moralesdm <morales.david.gr.22@gmail.com>
2025-06-25 21:14:48 -0500

    feat(init): 🎉initial files

--- src/test/java/com/project/autobackend4/AutoBackEnd4ApplicationTests.java
+++ src/test/java/com/project/autobackend4/AutoBackEnd4ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AutoBackEnd4ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
