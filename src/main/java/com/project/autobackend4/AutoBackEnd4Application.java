352416f120a252a614e607082cf4fa3488446f90
moralesdm <morales.david.gr.22@gmail.com>
2025-06-25 21:11:57 -0500

    first commit

---
+++ src/main/java/com/project/autobackend4/AutoBackEnd4Application.java
@@ -0,0 +1,13 @@
+package com.project.autobackend4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoBackEnd4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoBackEnd4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/autobackend4/AutoBackEnd4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.autobackend4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoBackEnd4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
